c68d79c9776bc829896c598171468e797e2bb2ca
AlexanderPhoenixtech <aclaros@megasur.es>
2024-08-08 10:16:32 +0200

    buscador de películas terminado

---
+++ projects/buscador-peliculas/src/services/movies.js
@@ -0,0 +1,16 @@
+export const  searchMovies = async ({search})=>{
+    if(search ==='') return null
+    try{
+        const response = await fetch(`https://www.omdbapi.com/?apikey=2fcdbe3d&s=${search}`)
+        const data = await response.json()
+        const movies = data.Search
+        return movies?.map((movie) => ({
+            id: movie.imdbID,
+            title: movie.Title,
+            year: movie.Year,
+            poster: movie.Poster
+          }));
+    }catch(e){
+        throw new Error('Error searching movies')
+    }
+}
